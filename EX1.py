242c998e0d814d6d82fca93fe998b1bce93c4dc5
MarcoRafaelSilvaGomes <marco.g@ua.pt>
2026-04-28 17:52:11 +0100

    Modifiquei o exercicio 1 de forma a ser separavel entre a obtenção dos dados dos histogramas e os plots.

--- EX1.py
+++ EX1.py
@@ -1,64 +1,60 @@
 import ROOT
+import sys
 
-# 1. Abrir o ficheiro
-f = ROOT.TFile.Open("ROOTFILES/AmberTarget_Run_0.root")
-edep_Per_Event = f.Get("edep_Per_Event") 
+def exercice1(filename, outputname):
+    # 1. Abrir o ficheiro
+    f = ROOT.TFile.Open(filename)
+    edep_Per_Event = f.Get("edep_Per_Event") 
 
-nbins = 400
+    nbins = 400
 
-# 1. Definir o mínimo
-# Como no seu Draw você faz o corte "detector > 0", o mínimo faz sentido ser 0.
-xmin = 0.0 
+    # 1. Definir o mínimo
+    # Como no seu Draw você faz o corte "detector > 0", o mínimo faz sentido ser 0.
+    xmin = 0.0 
 
-# 2. Descobrir o máximo global
-# Pedimos ao ROOT o máximo de CADA coluna explicitamente
-max0 = edep_Per_Event.GetMaximum("detector0")
-max1 = edep_Per_Event.GetMaximum("detector1")
-max2 = edep_Per_Event.GetMaximum("detector2")
-max3 = edep_Per_Event.GetMaximum("detector3")
+    # 2. Descobrir o máximo global
+    # Pedimos ao ROOT o máximo de CADA coluna explicitamente
+    max0 = edep_Per_Event.GetMaximum("detector0")
+    max1 = edep_Per_Event.GetMaximum("detector1")
+    max2 = edep_Per_Event.GetMaximum("detector2")
+    max3 = edep_Per_Event.GetMaximum("detector3")
 
-# O xmax será o maior valor absoluto entre os 4 detetores
-xmax = max(max0, max1, max2, max3) * 1.05
+    # O xmax será o maior valor absoluto entre os 4 detetores
+    xmax = max(max0, max1, max2, max3) * 1.05
 
-# 2. Criar o histograma com limites (0, 0)
-# Damos um nome interno "hist"
-histogram = ROOT.TH1F("hist", "Energia Total Acumulada;Energia (MeV);Entradas", nbins, xmin, xmax)
-histogram1 = ROOT.TH1F("hist1", "Energia Total Acumulada;Energia (MeV);Entradas", nbins, xmin, xmax)
-histogram2 = ROOT.TH1F("hist2", "Energia Total Acumulada;Energia (MeV);Entradas", nbins, xmin, xmax)
-histogram3 = ROOT.TH1F("hist3", "Energia Total Acumulada;Energia (MeV);Entradas", nbins, xmin, xmax)
+    # 2. Criar o histograma com limites (0, 0)
+    # Damos um nome interno "hist"
+    histogram = ROOT.TH1F("detetor0", "Energia Total Acumulada;Energia (MeV);Entradas", nbins, xmin, xmax)
+    histogram1 = ROOT.TH1F("detetor1", "Energia Total Acumulada;Energia (MeV);Entradas", nbins, xmin, xmax)
+    histogram2 = ROOT.TH1F("detetor2", "Energia Total Acumulada;Energia (MeV);Entradas", nbins, xmin, xmax)
+    histogram3 = ROOT.TH1F("detetor3", "Energia Total Acumulada;Energia (MeV);Entradas", nbins, xmin, xmax)
 
-# 3. Projetar os dados
-# O primeiro Draw define os eixos iniciais
-edep_Per_Event.Draw("detector0>>hist", "detector0 > 0", "goff")
-edep_Per_Event.Draw("detector1>>hist1", "detector1 > 0", "goff")
-edep_Per_Event.Draw("detector2>>hist2", "detector2 > 0", "goff")
-edep_Per_Event.Draw("detector3>>hist3", "detector3 > 0", "goff")
+    # 3. Projetar os dados
+    # O primeiro Draw define os eixos iniciais
+    edep_Per_Event.Draw("detector0>>detetor0", "detector0 > 0", "goff")
+    edep_Per_Event.Draw("detector1>>detetor1", "detector1 > 0", "goff")
+    edep_Per_Event.Draw("detector2>>detetor2", "detector2 > 0", "goff")
+    edep_Per_Event.Draw("detector3>>detetor3", "detector3 > 0", "goff")
 
-# 4. Visualização
-canvas = ROOT.TCanvas("c1", "Visualizacao", 800, 600)
-ROOT.gStyle.SetOptStat(0) 
-histogram.SetLineColor(ROOT.kBlue)      # Azul
-histogram1.SetLineColor(ROOT.kRed)       # Vermelho
-histogram2.SetLineColor(ROOT.kGreen + 2) # Verde escuro
-histogram3.SetLineColor(ROOT.kMagenta)   # Rosa/Roxo
+    ficheiro_saida = ROOT.TFile(outputname, "RECREATE")
+    histogram.Write()
+    histogram1.Write()
+    histogram2.Write()
+    histogram3.Write()
+    ficheiro_saida.Close()
 
-histogram.SetLineColor(ROOT.kBlue + 2)
-histogram.SetLineWidth(2)
-histogram.Draw("HIST") 
-histogram1.Draw("same") 
-histogram2.Draw("same") 
-histogram3.Draw("same") 
+    print("Histograma gerado com sucesso.")
 
-canvas.SetLogy()
-canvas.Update()
-# canvas.SaveAs("histograma_acumulado.png")
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Nenhum input inserido.")
+        exit(1)
 
-ficheiro_saida = ROOT.TFile("Meus_Histogramas_Energia.root", "RECREATE")
-histogram.Write()
-histogram1.Write()
-histogram2.Write()
-histogram3.Write()
-ficheiro_saida.Close()
+    for i in set(sys.argv[1:]):
+        if not i.isdigit() or len(i) != 1:
+            print(f"Erro: {i} não é um dígito.")
+            continue
 
-print("Histograma gerado com sucesso.")
-input("Pressione Enter para fechar...")
+        exercice1(f"ROOTFILES/AmberTarget_Run_{i}.root", f"ROOTFILES/EX1_HIST_{i}.root")
+
+    exit(0)
---
+++ EX1PLOTER.py
@@ -0,0 +1,68 @@
+import ROOT
+import sys
+import os
+
+def ex1ploter(filename):
+    if not os.path.isfile(filename):
+        print(f"Não existe o ficheiro {filename}.\nÉ melhor criá-lo para não haver problemas.")
+        return 1
+
+    colors = [ROOT.kBlue + 2, ROOT.kRed, ROOT.kGreen + 2, ROOT.kMagenta]
+
+    f = ROOT.TFile.Open(filename)
+
+    canvas = ROOT.TCanvas("c1", "Visualizacao", 800, 600)
+    ROOT.gStyle.SetOptStat(0)
+
+    legend = ROOT.TLegend(0.7, 0.7, 0.9, 0.9)
+
+    hist_names = [key.GetName() for key in f.GetListOfKeys()]
+
+    first = True
+
+    for i, name in enumerate(hist_names):
+        h = f.Get(name)
+
+        if i == 0:
+            h.SetLineWidth(2)
+
+        h.SetLineColor(colors[i])
+
+        h.GetXaxis().SetTitle("pZ (GeV)")
+        h.GetYaxis().SetTitle("Entradas")
+        #h.GetXaxis().SetRangeUser(config.xmin, config.xmax)
+
+        if first:
+            h.Draw("HIST")
+            first = False
+        else:
+            h.Draw("HIST SAME")
+
+        h.SetMinimum(1)
+        #h.SetMaximum(1e5)
+
+        legend.AddEntry(h, name, "l")
+
+    legend.Draw()
+
+    canvas.SetLogy()
+    canvas.Update()
+    # canvas.SaveAs("histograma_acumulado.png")
+
+    input("Enter para sair")
+
+    return 0
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Nenhum input inserido.")
+        exit(1)
+
+    for i in set(sys.argv[1:]):
+        if not i.isdigit() or len(i) != 1:
+            print(f"Erro: {i} não é um dígito.")
+            continue
+
+        ex1ploter(f"ROOTFILES/EX1_HIST_{i}.root")
+
+    exit(0)
