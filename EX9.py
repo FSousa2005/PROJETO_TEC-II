d60cfff35801618c547ae8b3b3ff07dfc3c9d5b9
MarcoRafaelSilvaGomes <marco.g@ua.pt>
2026-04-28 15:51:21 +0100

    Completei o exercício 9 com o seu ploter. Em sérias dúvidas para continuar o ploter geral.

--- EX9.py
+++ EX9.py
@@ -1,11 +1,9 @@
 import sys
 import ROOT
 
-def exercice9(filename):
+def exercice9(filename, outputname):
     # Abrir ficheiro
     f = ROOT.TFile.Open(filename)
-
-    # Escolher a TTree correta (recomendo tracksData)
     tree = f.Get("tracksData")
 
     nbins = 200
@@ -13,63 +11,39 @@ def exercice9(filename):
     xmax = 210
 
     # Criar histogramas
-    h_primary = ROOT.TH1F("h_primary", "pZ - Pioes Primarios; pZ (GeV); Entradas", nbins, xmin, xmax)
-    h_secondary = ROOT.TH1F("h_secondary", "pZ - Pioes Secundarios; pZ (GeV); Entradas", nbins, xmin, xmax)
+    h_primary = ROOT.TH1F("h_primary", "pZ - Pioes Primarios", nbins, xmin, xmax)
+    h_secondary = ROOT.TH1F("h_secondary", "pZ - Pioes Secundarios", nbins, xmin, xmax)
 
-    # Cortes (cuts)
+    # Cortes
     cut_pions = "(particlePDG == 211 || particlePDG == -211)"
     cut_primary = "IsPrimary == 1"
     cut_secondary = "IsPrimary == 0"
 
-    # Projetar dados
+    # Preencher histogramas SEM desenhar
     tree.Draw("pZ_GeV >> h_primary", f"{cut_pions} && {cut_primary}", "goff")
     tree.Draw("pZ_GeV >> h_secondary", f"{cut_pions} && {cut_secondary}", "goff")
-    ROOT.gStyle.SetOptStat(0)
 
-    # Visualização
-    canvas = ROOT.TCanvas("c1", "Distribuicao pZ", 800, 600)
+    # Criar ficheiro de saída
+    ficheiro_saida = ROOT.TFile(outputname, "RECREATE")
 
-    h_primary.SetLineColor(ROOT.kBlue)
-    h_secondary.SetLineColor(ROOT.kRed)
+    # Guardar histogramas
+    h_primary.Write()
+    h_secondary.Write()
 
-    h_primary.SetLineWidth(2)
-    h_secondary.SetLineWidth(2)
-
-    h_primary.SetMinimum(1)     # valor mínimo no eixo Y
-    h_primary.SetMaximum(1e5)   # valor máximo no eixo Y
-
-    h_primary.Draw("HIST")
-    h_secondary.Draw("HIST SAME")
-
-    # Legenda
-    legend = ROOT.TLegend(0.7, 0.7, 0.9, 0.9)
-    legend.AddEntry(h_primary, "Primarios", "l")
-    legend.AddEntry(h_secondary, "Secundarios", "l")
-    legend.Draw()
-
-    canvas.SetLogy()
-    canvas.Update()
-
-    ficheiro_saida = ROOT.TFile("Meus_Histogramas_Energia.root", "RECREATE")
-    histogram.Write()
-    histogram1.Write()
-    histogram2.Write()
-    histogram3.Write()
     ficheiro_saida.Close()
+    f.Close()
 
-    input("Pressione Enter para fechar...")
 
-# Exemplo de uso
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("Nenhum input inserido.")
         exit(1)
 
     for i in set(sys.argv[1:]):
-        if not i.isdigit() or len(i)!=1:
+        if not i.isdigit() or len(i) != 1:
             print(f"Erro: {i} não é um dígito.")
             continue
 
-        exercice9(f"ROOTFILES/AmberTarget_Run_{i}.root")
+        exercice9(f"ROOTFILES/AmberTarget_Run_{i}.root", f"ROOTFILES/EX9_HIST_{i}.root")
 
     exit(0)
